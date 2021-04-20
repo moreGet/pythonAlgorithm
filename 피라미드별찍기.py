5a5b824692a1ac82963445942de85b4a95098d6a
dkdlwmzhs <dkdlwmzhs@naver.com>
2021-04-20 22:30:43 +0900

    UPDATE

--- 피라미드별찍기.py
+++ 피라미드별찍기.py
@@ -1,11 +1,10 @@
-floor = 6
+floor = 10
 
-for i in range(1, floor):
-    for j in range(1, floor-i):
+for i in range(1, floor+1): # 1 시작 이므로 +1을 해줘야함
+    for j in range(floor-i):
         print(" ", end="")
 
-    for j in range(1, int(floor/2)*i-1):
+    for j in range(i*2-1):
         print("*", end="")
-
-    print()
     
+    print()
